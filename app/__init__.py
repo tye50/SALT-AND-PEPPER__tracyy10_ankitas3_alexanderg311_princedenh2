f2d88457c9e5baff70b1eab58dc89efed15c7040
Tracy Ye <tye50@stuy.edu>
2025-04-01 10:50:07 -0400

    test D0

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask,render_template
+from flask import render_template
+from flask import request
+from flask import session
+from flask import redirect
+from flask import url_for
+from flask import flash
+import sqlite3
+
+@app.route("/")
+def main():
+    return render_template("main.html")
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
