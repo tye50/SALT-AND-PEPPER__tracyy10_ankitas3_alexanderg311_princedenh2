46c0f444be44e648e5e1e7dd186b242af937eb1f
Ankita Saha <76600603+ankita320@users.noreply.github.com>
2025-04-02 13:58:08 -0400

    Update Data_cleaning.py

--- app/Data_cleaning.py
+++ app/Data_cleaning.py
@@ -1,1 +1,15 @@
+import pandas as pd
 
+from google.colab import files
+fake_data = files.upload()
+
+fake_data = pd.read_csv('Fake.csv')
+print(fake_data.head())
+
+#data cleaning remove duplicate null etc
+fake_data = fake_data.drop_duplicates()
+fake_data = fake_data.dropna()
+fake_data.dropna(inplace = True)
+fake_data = pd.read_csv('Fake.csv')
+print(fake_data.head())
+fake_data.to_csv('Fake.csv', index=False)
