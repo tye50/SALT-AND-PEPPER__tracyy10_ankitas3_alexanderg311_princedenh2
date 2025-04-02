9db77e79add2bb84278af082c803fb0fde55e7e5
Princeden Hom <phom50@stuy.edu>
2025-04-02 11:01:23 -0400

    started classification

--- app/news_analysis.py
+++ app/news_analysis.py
@@ -4,40 +4,45 @@ import os
 
 fake = pd.read_csv(os.path.abspath('data/Fake.csv'))
 real = pd.read_csv(os.path.abspath('data/True.csv'))
-fake_text = fake['text']
-fake_title = fake['title']
-real_text = real['text']
-real_title = real['title']
-fake_text_dict = {}
-fake_title_dict = {}
-real_text_dict = {}
-real_title_dict = {}
-for i in range(0, len(fake_text)):
-    title_words = fake_title[i].split(' ')
-    text_words = fake_text[i].split(' ')
-    for word in title_words:
-        if word in fake_title_dict.keys():
-            fake_title_dict[word] += 1
-        else:
-            fake_title_dict[word] = 1
-    for word in text_words:
-        if word in fake_text_dict.keys():
-            fake_text_dict[word] += 1
-        else:
-            fake_text_dict[word] = 1
-for i in range(0, len(real_text)):
-    title_words = real_title[i].split(' ')
-    text_words = real_text[i].split(' ')
-    for word in title_words:
-        if word in real_title_dict.keys():
-            real_title_dict[word] += 1
-        else:
-            real_title_dict[word] = 1
-    for word in text_words:
-        if word in real_text_dict.keys():
-            real_text_dict[word] += 1
-        else:
-            real_text_dict[word] = 1
-print(len(real_text_dict))
-print(len(real_title_dict))
+fake['true'] = 0
+real['true'] = 1
+news = pd.concat([fake, real], ignore_index = True)
 print(fake.head())
+print(real.head())
+print(news.head())
+def words_counts():
+    fake_text = fake['text']
+    fake_title = fake['title']
+    real_text = real['text']
+    real_title = real['title']
+    fake_text_dict = {}
+    fake_title_dict = {}
+    real_text_dict = {}
+    real_title_dict = {}
+    for i in range(0, len(fake_text)):
+        title_words = fake_title[i].split(' ')
+        text_words = fake_text[i].split(' ')
+        for word in title_words:
+            if word in fake_title_dict.keys():
+                fake_title_dict[word] += 1
+            else:
+                fake_title_dict[word] = 1
+        for word in text_words:
+            if word in fake_text_dict.keys():
+                fake_text_dict[word] += 1
+            else:
+                fake_text_dict[word] = 1
+    for i in range(0, len(real_text)):
+        title_words = real_title[i].split(' ')
+        text_words = real_text[i].split(' ')
+        for word in title_words:
+            if word in real_title_dict.keys():
+                real_title_dict[word] += 1
+            else:
+                real_title_dict[word] = 1
+        for word in text_words:
+            if word in real_text_dict.keys():
+                real_text_dict[word] += 1
+            else:
+                real_text_dict[word] = 1
+
