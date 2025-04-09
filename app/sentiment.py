cc0c522ff363fb4275a835dab006dd55095dfe3e
Tracy Ye <tye50@stuy.edu>
2025-04-09 11:01:55 -0400

    e

---
+++ app/sentiment.py
@@ -0,0 +1,5 @@
+ https://text2data.com/Integration
+ 
+ https://github.com/shabisht/Sentiment-Analysis-API
+ 
+ https://cloud.google.com/natural-language/docs/analyzing-sentiment#language-sentiment-string-python
