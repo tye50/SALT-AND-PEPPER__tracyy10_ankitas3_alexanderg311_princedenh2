c82053988c24002b823a2e60099e1419818c2539
Tracy Ye <tye50@stuy.edu>
2025-04-10 11:01:32 -0400

    jwnedkas

--- app/db.py
+++ app/db.py
@@ -1,57 +1,27 @@
 import sqlite3
 
+def connect():
+    db = sqlite3.connect("SALT.db")
+    c = db.cursor()
+    return c, db
 
-USER_FILE = "SALT.db"
+def close(db):
+    db.commit()
+    db.close()
+
 def build():
-    createUsers()
-    createArticleInfo()
-    createWordCountInfo()
-    createFontSizeInfoF()
-    createFontSizeInfoR()
-    #c.execute("CREATE TABLE IF NOT EXISTS loaded_articles(link TEXT, rating_percentage INTEGER)")
-    
-    
-    
-def createUsers():
-    users = sqlite3.connect(USER_FILE)
-    c = users.cursor()
-    command = "CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT)"
-    c.execute(command)
-    users.commit()
-    
-def createArticleInfo():
-    articles = sqlite3.connect(USER_FILE)
-    c = articles.cursor()
-    c.execute("CREATE TABLE IF NOT EXISTS articles(title TEXT, content TEXT)")
-    c.execute(command)
-    articles.commit()
-    
-def createWordCountInfoF():
-    wordCountF = sqlite3.connect(USER_FILE)
-    c = wordCountF.cursor()
-    c.execute("CREATE TABLE IF NOT EXISTS wordCountF(word TEXT, count INT)")
-    wordCountF.commit()
-    
-def createWordCountInfoR():
-    wordCountR = sqlite3.connect(USER_FILE)
-    c = wordCountR.cursor()
-    c.execute("CREATE TABLE IF NOT EXISTS wordCountR(word TEXT, count INT)")
-    wordCountR.commit()
-    
-def addWordCountInfoF(word, count):
-    wordCountF = sqlite3.connect(USER_FILE)
-    c = wordCountF.cursor()
-    c.execute("INSERT INTO wordCountF(word, count) VALUES (?, ?)", (word, count))
-    wordCountF.commit()
-    
-def addWordCountInfoR(word, count):
-    wordCountR = sqlite3.connect(USER_FILE)
-    c = wordCountR.cursor()
-    c.execute("INSERT INTO wordCountR(word, count) VALUES (?, ?)", (word, count))
-    wordCountR.commit()
-    
+    c,db = connect()
+    c.execute("CREATE TABLE IF NOT EXISTS fake_words(word TEXT, count INTEGER)")
+    c.execute("CREATE TABLE IF NOT EXISTS true_words(word TEXT, count INTEGER)")
+    c.execute("CREATE TABLE IF NOT EXISTS users(username TEXT, password TEXT)")
+    close(db)
+    print("Database successfully created.")
 
 def updateCountF(word):
+    c, db = connect()
+    c.execute("SELECT count FROM wordCountF WHERE word = ?", (word,))
+    
+    
     wordCountF = sqlite3.connect(USER_FILE)
     c = wordCountF.cursor()
     c.execute("SELECT count FROM wordCountF WHERE word = ?", (word,))
@@ -59,81 +29,3 @@ def updateCountF(word):
     countF = int(data[0])+1
     c.execute("UPDATE wordCountF SET count = ? WHERE word = ?", (countF, word))
     wordCountF.commit()
-
-def updateCountR(word):
-    wordCountR = sqlite3.connect(USER_FILE)
-    c = wordCountR.cursor()
-    c.execute("SELECT count FROM wordCountR WHERE word = ?", (word,))
-    data = c.fetchone()
-    countR = int(data[0])+1
-    c.execute("UPDATE wordCountR SET count = ? WHERE word = ?", (countR, word))
-    wordCountR.commit()
-    
-def returnWordCountF():
-    wordCountF = sqlite3.connect(USER_FILE)
-    c = wordCountF.cursor()
-    c.execute("SELECT * FROM wordCountF")
-    return c.fetchall()
-
-def returnWordCountR():
-    wordCountF = sqlite3.connect(USER_FILE)
-    c = wordCountF.cursor()
-    c.execute("SELECT * FROM wordCountF")
-    return c.fetchall()
-    
-def updateArticleInfo(username, title, content):
-    users = sqlite3.connect(USER_FILE)
-    c = users.cursor()
-    if (c.execute("SELECT 1 FROM articles WHERE username=?", (username,))).fetchone() == None:
-        return
-    c.execute("UPDATE webinfo SET title=?, content=? WHERE username=?", (title, content, username))
-    users.commit()
-    
- 
-def createFontSizeInfoF():
-    fontsF = sqlite3.connect(USER_FILE)
-    c = fontsF.cursor()
-    c.execute("CREATE TABLE IF NOT EXISTS fontsF(word TEXT, size REAL)")
-    fontsF.commit()
-    
-def createFontSizeInfoR():
-    fontsR = sqlite3.connect(USER_FILE)
-    c = fontsR.cursor()
-    c.execute("CREATE TABLE IF NOT EXISTS fontsR(word TEXT, size REAL)")
-    fontsR.commit()
-    
-def addFontSizeInfoF(word, size):
-    fontsF = sqlite3.connect(USER_FILE)
-    c = fontsF.cursor()
-    c.execute("INSERT INTO fontsF(word, size) VALUES (?, ?)", (word, size))
-    fontsF.commit()
-
-def addFontSizeInfoR(word, size):
-    fontsR = sqlite3.connect(USER_FILE)
-    c = fontsR.cursor()
-    c.execute("INSERT INTO fontsR(word, size) VALUES (?, ?)", (word, size))
-    fontsR.commit()
-
-def returnFontTableF():
-    fontsF = sqlite3.connect(USER_FILE)
-    c = fontsF.cursor()
-    c.execute("SELECT * FROM fontsF")
-    return c.fetchall()
-
-def returnFontTableR():
-    fontsR = sqlite3.connect(USER_FILE)
-    c = fontsR.cursor()
-    c.execute("SELECT * FROM fontsR")
-    return c.fetchall()
-
-def deleteFontTableF():
-    fontsF = sqlite3.connect(USER_FILE)
-    c = fontsF.cursor()
-    c.execute("DELETE FROM fontsF")
-    fontsF.commit()
-    
-def deleteFontTableR():
-    fontsR = sqlite3.connect(USER_FILE)
-    c = fontsR.cursor()
-    c.execute("DELETE FROM fontsR")
-    fontsR.commit()
