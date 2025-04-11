ff24ccf92a335fee038ad3a36deff9c75eaa9bf5
Tracy Ye <tye50@stuy.edu>
2025-04-11 06:03:23 -0400

    fix

--- app/loadOnce.py
+++ app/loadOnce.py
@@ -8,8 +8,8 @@ build()
 with open('data/Fake (1).csv', 'r', encoding="utf8") as file:
      text=csv.reader(file)
      for row in text:
-         row = row.strip().lower().replace(",", " ").replace(".", " ").replace("!", " ").replace("?", " ").replace(";", " ").replace("'", " ").replace('"', " ").replace('(', " ").replace(')', " ").replace('[', " ").replace(']', " ").replace('{', " ").replace('}', " ").replace(',', " ").replace('.', " ").replace('-', " ").replace('_', " ")
-         words = row[1].split()
+         row = row[1].strip().lower().replace(",", " ").replace(".", " ").replace("!", " ").replace("?", " ").replace(";", " ").replace("'", " ").replace('"', " ").replace('(', " ").replace(')', " ").replace('[', " ").replace(']', " ").replace('{', " ").replace('}', " ").replace(',', " ").replace('.', " ").replace('-', " ").replace('_', " ")
+         words = row.split()
          for i in words:
              if len(wordInfoF(i)) == 0:
                  addWordF(i)
@@ -21,8 +21,8 @@ with open('data/Fake (1).csv', 'r', encoding="utf8") as file:
 with open('data/True (1).csv', 'r', encoding="utf8") as file:
      text=csv.reader(file)
      for row in text:
-         row = row.strip().lower().replace(",", " ").replace(".", " ").replace("!", " ").replace("?", " ").replace(";", " ").replace("'", " ").replace('"', " ").replace('(', " ").replace(')', " ").replace('[', " ").replace(']', " ").replace('{', " ").replace('}', " ").replace(',', " ").replace('.', " ").replace('-', " ").replace('_', " ")
-         words = row[1].split()
+         row = row[1].strip().lower().replace(",", " ").replace(".", " ").replace("!", " ").replace("?", " ").replace(";", " ").replace("'", " ").replace('"', " ").replace('(', " ").replace(')', " ").replace('[', " ").replace(']', " ").replace('{', " ").replace('}', " ").replace(',', " ").replace('.', " ").replace('-', " ").replace('_', " ")
+         words = row.split()
          for i in words:
              if len(wordInfoT(i)) == 0:
                  addWordT(i)
