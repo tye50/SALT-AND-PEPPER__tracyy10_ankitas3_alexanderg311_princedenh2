599c1f7cd641f20268b18a92c28f6c049e3d0233
Tracy Ye <tye50@stuy.edu>
2025-04-15 13:49:20 -0400

    new_main

--- app/loadOnce.py
+++ app/loadOnce.py
@@ -8,28 +8,29 @@ build()
 with open('data/Fake (1).csv', 'r', encoding="utf8") as file:
      text=csv.reader(file)
      for row in text:
-         row = row[1].strip().lower().replace(",", " ").replace(".", " ").replace("!", " ").replace("?", " ").replace(";", " ").replace("'", " ").replace('"', " ").replace('(', " ").replace(')', " ").replace('[', " ").replace(']', " ").replace('{', " ").replace('}', " ").replace(',', " ").replace('.', " ").replace('-', " ").replace('_', " ")
+         row = row[1].strip().lower().replace("/", " ").replace(":", " ").replace(",", " ").replace(".", " ").replace("!", " ").replace("?", " ").replace(";", " ").replace("'", "").replace('"', " ").replace('(', " ").replace(')', " ").replace('[', " ").replace(']', " ").replace('{', " ").replace('}', " ").replace(',', " ").replace('.', " ").replace('-', "").replace('_', " ")
          words = row.split()
          for i in words:
              if wordCountF(i) == None:
                  addWordF(i)
-                 print("added word: " + i + " to FALSE")
+                 #print("added word: " + i + " to FALSE")
              else:
                  updateCountF(i)
-                 print("updated: " + i + " in FALSE")
+                 #print("updated: " + i + " in FALSE")
+print("Finished populating fake_words")
 
 # populate true
 with open('data/True (1).csv', 'r', encoding="utf8") as file:
      text=csv.reader(file)
      for row in text:
-         row = row[1].strip().lower().replace(",", " ").replace(".", " ").replace("!", " ").replace("?", " ").replace(";", " ").replace("'", " ").replace('"', " ").replace('(', " ").replace(')', " ").replace('[', " ").replace(']', " ").replace('{', " ").replace('}', " ").replace(',', " ").replace('.', " ").replace('-', " ").replace('_', " ")
+         row = row[1].strip().lower().replace("/", " ").replace(":", " ").replace(",", " ").replace(".", " ").replace("!", " ").replace("?", " ").replace(";", " ").replace("'", "").replace('"', " ").replace('(', " ").replace(')', " ").replace('[', " ").replace(']', " ").replace('{', " ").replace('}', " ").replace(',', " ").replace('.', " ").replace('-', "").replace('_', " ")
          words = row.split()
          for i in words:
              if wordCountT(i) == None:
                  addWordT(i)
-                 print("added word: " + i + " to TRUE")
+                 #print("added word: " + i + " to TRUE")
              else:
                  updateCountT(i)
-                 print("updated: " + i + " in TRUE")
+                 #print("updated: " + i + " in TRUE")
 
 ##################################  DO NOT RUN THIS FILE  #########################################
