b42b0fbeb0abdf5a790b589ea7fd9ebaf79566f3
princeden <princedenhom@gmail.con>
2025-04-19 18:30:59 -0400

    update classifier

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,9 @@ import os
 import random
 import csv
 import sqlite3
-
-
+import news_classifier
+import pickle
+import news_scrape
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -82,11 +83,22 @@ def search():
     # plt.savefig('torf.png')
     return render_template("search.html", query=query, count=count, words=words, countR=countR, wordsR=wordsR)
 
-@app.route("/analyze")
+@app.route("/analyze", methods=['GET', 'POST'])
 def analyze():
     if not 'username' in session:
         flash("Please log in to access this page.")
         return redirect("/")
+    if request.method == 'POST':
+        link = request.form['link']
+        try:
+            text = news_scrape.get_text(link)
+            p = news_classifier.get_probability(model, text)
+            return render_template("analyze.html", link=link, text=text, p_true=p[0],p_false=p[1])
+        except Exception as e:
+            print(e)
+            flash('Invalid URL or unable to fetch the article. Please try again.')
+            return render_template("analyze.html", link=None, text=None, p_true=None, p_false=None)
+    return render_template("analyze.html",link=None, text=None, p_true=None, p_false=None)
     return render_template("analyze.html")
 
 @app.route("/generate")
@@ -94,6 +106,7 @@ def generate():
     if not 'username' in session:
         flash("Please log in to access this page.")
         return redirect("/")
+    
     return render_template("generate.html")
 
 @app.route("/logout")
@@ -103,4 +116,7 @@ def logout():
     
 if __name__ == "__main__":
     app.debug = True
+    model = pickle.load(open("finalized_model.sav", "rb"))
+    news_classifier.fit_tfidf_vectorizer()
+    print("Model loaded")
     app.run()
--- app/news_classifier.py
+++ app/news_classifier.py
@@ -12,6 +12,23 @@ from sklearn.ensemble import RandomForestClassifier
 import pickle
 from sklearn.naive_bayes import GaussianNB
 tfidf_vectorizer = TfidfVectorizer()
+def fit_tfidf_vectorizer():
+    fake = pd.read_csv(os.path.abspath('data/Fake (1).csv'))
+    real = pd.read_csv(os.path.abspath('data/True (1).csv'))
+
+    fake['true'] = 0
+    real['true'] = 1
+    news = pd.concat([fake, real], ignore_index = True)
+    news.drop(['subject', 'date'], axis=1, inplace=True)
+
+    # print(news.head(10))
+    # print(news.columns)
+    x = news[['title', 'text']]
+    y = news['true']
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+    X_train_tfidf = tfidf_vectorizer.fit_transform(X_train["title"] + " " + X_train["text"])
+    # print(X_train_tfidf)
+    X_test_tfidf = tfidf_vectorizer.transform(X_test["title"] + " " + X_test["text"])
 def get_model():
     fake = pd.read_csv(os.path.abspath('data/Fake (1).csv'))
     real = pd.read_csv(os.path.abspath('data/True (1).csv'))
@@ -21,18 +38,18 @@ def get_model():
     news = pd.concat([fake, real], ignore_index = True)
     news.drop(['subject', 'date'], axis=1, inplace=True)
 
-    print(news.head(10))
-    print(news.columns)
+    # print(news.head(10))
+    # print(news.columns)
     x = news[['title', 'text']]
     y = news['true']
     X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
     X_train_tfidf = tfidf_vectorizer.fit_transform(X_train["title"] + " " + X_train["text"])
-    print(X_train_tfidf)
+    # print(X_train_tfidf)
     X_test_tfidf = tfidf_vectorizer.transform(X_test["title"] + " " + X_test["text"])
 
 
-    print(x)
-    print(y)
+    # print(x)
+    # print(y)
 
 
 
@@ -47,8 +64,8 @@ def get_model():
 def get_probability(model, text):
     text = str(text)
     prediction = model.predict_proba(tfidf_vectorizer.transform([text]))
-    return prediction[0];
-classifier = get_model()
-n = get_probability(classifier, """trump""")
-print(n)
+    return prediction[0]
+# classifier = get_model()
+# n = get_probability(classifier, """trump""")
+# print(n)
 
