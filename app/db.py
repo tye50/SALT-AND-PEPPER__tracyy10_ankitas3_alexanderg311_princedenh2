5ab68daca5d19d6ee5d02a02bd25c9970ec74b53
Tracy Ye <tye50@stuy.edu>
2025-04-20 14:34:00 -0400

    normalized

--- app/db.py
+++ app/db.py
@@ -1,7 +1,7 @@
 import sqlite3
 import json
 import random
-
+import copy
 
 def connect():
     db = sqlite3.connect("SALT.db")
@@ -133,7 +133,8 @@ def getRandomFakeWord():
 # build()
 
 #########################################################################################
-    
+
+filler = {'call', "eye","the", "a", "that", "he", "her", "his", "him", "if", "they",'upon', 'still', 'nevertheless', 'down', 'every', 'forty', '‘re', 'always', 'whole', 'side', "n't", 'now', 'however', 'an', 'show', 'least', 'give', 'below', 'did', 'sometimes', 'which', "'s", 'nowhere', 'per', 'hereupon', 'yours', 'she', 'moreover', 'eight', 'somewhere', 'within', 'whereby', 'few', 'has', 'so', 'have', 'for', 'noone', 'top', 'were', 'those', 'thence', 'eleven', 'after', 'no', '’ll', 'others', 'ourselves', 'themselves', 'though', 'that', 'nor', 'just', '’s', 'before', 'had', 'toward', 'another', 'should', 'herself', 'and', 'these', 'such', 'elsewhere', 'further', 'next', 'indeed', 'bottom', 'anyone', 'his', 'each', 'then', 'both', 'became', 'third', 'whom', '‘ve', 'mine', 'take', 'many', 'anywhere', 'to', 'well', 'thereafter', 'besides', 'almost', 'front', 'fifteen', 'towards', 'none', 'be', 'herein', 'two', 'using', 'whatever', 'please', 'perhaps', 'full', 'ca', 'we', 'latterly', 'here', 'therefore', 'us', 'how', 'was', 'made', 'the', 'or', 'may', '’re', 'namely', "'ve", 'anyway', 'amongst', 'used', 'ever', 'of', 'there', 'than', 'why', 'really', 'whither', 'in', 'only', 'wherein', 'last', 'under', 'own', 'therein', 'go', 'seems', '‘m', 'wherever', 'either', 'someone', 'up', 'doing', 'on', 'rather', 'ours', 'again', 'same', 'over', '‘s', 'latter', 'during', 'done', "'re", 'put', "'m", 'much', 'neither', 'among', 'seemed', 'into', 'once', 'my', 'otherwise', 'part', 'everywhere', 'never', 'myself', 'must', 'will', 'am', 'can', 'else', 'although', 'as', 'beyond', 'are', 'too', 'becomes', 'does', 'a', 'everyone', 'but', 'some', 'regarding', '‘ll', 'against', 'throughout', 'yourselves', 'him', "'d", 'it', 'himself', 'whether', 'move', '’m', 'hereafter', 're', 'while', 'whoever', 'your', 'first', 'amount', 'twelve', 'serious', 'other', 'any', 'off', 'seeming', 'four', 'itself', 'nothing', 'beforehand', 'out', 'very', 'already', 'various', 'until', 'hers', 'they', 'not', 'them', 'where', 'would', 'since', 'everything', 'at', 'together', 'yet', 'more', 'six', 'back', 'with', 'thereupon', 'becoming', 'around', 'due', 'keep', 'somehow', 'n‘t', 'across', 'all', 'when', 'i', 'empty', 'nine', 'five', 'get', 'see', 'been', 'name', 'between', 'hence', 'ten', 'several', 'from', 'whereupon', 'through', 'hereby', "'ll", 'alone', 'something', 'formerly', 'without', 'above', 'onto', 'except', 'enough', 'become', 'behind', '’d', 'its', 'most', 'n’t', 'might', 'whereas', 'anything', 'if', 'her', 'via', 'fifty', 'is', 'thereby', 'twenty', 'often', 'whereafter', 'also', 'anyhow', 'cannot', 'our', 'could', 'because', 'who', 'beside', 'by', 'whence', 'being', 'meanwhile', 'this', 'afterwards', 'whenever', 'mostly', 'what', 'one', 'nobody', 'seem', 'less', 'do', '‘d', 'say', 'thus', 'unless', 'along', 'yourself', 'former', 'thru', 'he', 'hundred', 'three', 'sixty', 'me', 'sometime', 'whose', 'you', 'quite', '’ve', 'about', 'even'}
 t = returnT()
 f = returnF()
 
@@ -141,12 +142,14 @@ all_dictF=[]
 all_dictT=[]
 
 for i in range(0,len(t)):
-    single_dict = dict(name=t[i][0], count=(t[i][1]), color=t[i][2])
-    all_dictT.append(single_dict)
+    if t[i][0] not in filler:
+        single_dict = dict(name=t[i][0], count=(t[i][1]), color=t[i][2])
+        all_dictT.append(single_dict)
 
 for i in range(0,len(f)):
-    single_dict = dict(name=f[i][0], count=(f[i][1]), color=f[i][2])
-    all_dictF.append(single_dict)
+    if f[i][0] not in filler:
+        single_dict = dict(name=f[i][0], count=(f[i][1]), color=f[i][2])
+        all_dictF.append(single_dict)
     
 all_dictT.extend(all_dictF)
 
@@ -154,10 +157,57 @@ with open("pleasework.json", "w") as f:
     json.dump(all_dictT, f)
 
 
+def change_size(modify_dict):
+    total_dict = all_dictF + all_dictT
+    hi=total_dict[0].get("count")
+    lo=total_dict[0].get("count")
+    for i in total_dict:
+        if i.get("count")>hi:
+            hi=i.get("count")
+        elif i.get("count")<lo:
+            lo=i.get("count")
+    diff = hi-lo
+    rate = diff/1000
+    for i in modify_dict:
+        font_size=(i.get("count")/hi)*100
+        if font_size <= .01:
+            i["count"] = 10
+        elif font_size> .01 and font_size<= .02:
+            i["count"] = 15
+        elif font_size> .02 and font_size<= .05:
+            i["count"] = 20
+        elif font_size> .05 and font_size<= .1:
+            i["count"] = 30
+        elif font_size> .1 and font_size<= .3:
+            i["count"] = 45
+        elif font_size> .4 and font_size<= .5:
+            i["count"] = 60
+        elif font_size> .5 and font_size<= 2:
+            i["count"] = 65
+        elif font_size> 2 and font_size<= 5:
+            i["count"] = 70
+        elif font_size> 5 and font_size<= 10:
+            i["count"] = 75
+        elif font_size> lo+(4*rate) and font_size<= lo + (5*rate):
+            i["count"] = 80
+        elif font_size> lo+(5*rate) and font_size<= lo + (6*rate):
+            i["count"] = 85
+        elif font_size> lo+(6*rate) and font_size<= lo + (7*rate):
+            i["count"] = 90
+        elif font_size> lo+(7*rate) and font_size<= lo + (8*rate):
+            i["count"] = 95
+        elif font_size> lo+(8*rate) and font_size<= lo + (9*rate):
+            i["count"] = 100
+        elif font_size> lo+(9*rate) and font_size<= lo + (10*rate):
+            i["count"] = 110
+        elif font_size> lo+(10*rate) and font_size<= lo + (11*rate):
+            i["count"] = 120
+        else:
+            i["count"] = 150
+    return modify_dict
 
-
-
-
+all_dictF = change_size(copy.deepcopy(all_dictF))
+all_dictT = change_size(copy.deepcopy(all_dictT))
 
 
 
